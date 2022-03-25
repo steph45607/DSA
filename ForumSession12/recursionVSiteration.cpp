2bde2f0ae603cc191012da7058d0d4435855906e
steph45607 <staniswinata.stephanie@gmail.com>
2022-03-25 22:49:34 +0700

    1 done

---
+++ ForumSession12/recursionVSiteration.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int iteration(int n){
+    int result = 1;
+    for(int i = 1; i <= n; i++){
+        result *= i;
+    }
+    return result;
+}
+
+int recursion(int n){
+    if(n <= 1){
+        return 1;
+    }
+    else{
+        return n * recursion(n-1);
+    }
+}
+
+int main(){
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    cout << "With iteration: " << iteration(n) << endl;
+    cout << "With recursion: " << recursion(n) << endl;
+}
